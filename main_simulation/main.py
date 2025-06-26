16d2affeb03fe45c50452d8a159b6fc664f8b7cc
Marcello Costa <marcello.costa.br@ieee.org>
2025-06-26 18:53:17 +0200

    "logs"

--- main_simulation/main.py
+++ main_simulation/main.py
@@ -185,7 +185,7 @@ if __name__ == "__main__":
 
      
     test_type = ['MSAR', 'MSARk', 'MSARkh']
-    test_type  = test_type[0]
+    test_type  = test_type[2]
     K = 9
     N = 250
     msarSim(test_type, N, K)
